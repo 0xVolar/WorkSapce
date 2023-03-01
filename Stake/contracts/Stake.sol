c4dda073eab5a7ad3de7e547a45f3bbfc7da15ec
zhaoyang <zhao_volar@163.com>
2023-03-01 19:18:02 +0800

    update

--- Stake/contracts/Stake.sol
+++ Stake/contracts/Stake.sol
@@ -7,19 +7,8 @@ import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Stake is Ownable, ReentrancyGuard {
-    using SafeMath for uint;
-
     // BSC 链 USDC 地址
     IERC20 public usdc = IERC20(0x8965349fb649A33a30cbFDa057D8eC2C48AbE2A2);
-
-    // struct Order {
-    //     address user;
-    //     uint amount;
-    //     uint updateTime;  //最近更新时间，如果已经计算过收益之后变为计算收益的时间
-    //     uint endTime;       
-    //     uint amountOfDevice;
-    //     uint yieldPerDay;
-    // }
 
     struct Order {
         address user;
@@ -44,15 +33,17 @@ contract Stake is Ownable, ReentrancyGuard {
     uint private yeildOfAdmin;
 
     //用户的收益
-    mapping(address => uint) yieldOfUser;
+    mapping(bool => mapping(address => uint)) yieldOfUser;
     //用户的本金
-    mapping(address => uint) balanceOf;
+    mapping(bool => mapping(address => uint)) balanceOf;
     //orders中对应的订单在用户列表中的位置
     mapping(uint => uint) orderInUserList;
     //用户拥有的订单数量
     mapping(address => uint) orderOfUser;
     //用户所拥有的订单的位置
     mapping(address => mapping(uint => uint)) indexOfUser;
+    //佣金
+    mapping(address => uint) commission;
 
     event Depoist(address user, uint amount, uint current_time, uint lockDuration);
     event GetReward(address user, uint amount);
@@ -70,21 +61,23 @@ contract Stake is Ownable, ReentrancyGuard {
      * @param _invitation 上级邀请人
      * @param _method 为 false 表示质押 15 天，true 表示质押 30 天
      */
-    function depoist(uint _amount, address _invitation, bool _method) public returns() {
+    function depoist(uint _amount, address _invitation, bool _method) public {
         (uint current_totoalDevice, uint current_totalYeild) = _getTotalDeviceAndYield();   // gas saving
+        uint expend = _method == true ? expend_30 : expend_15;
         require(current_totoalDevice * current_totalYeild != 0, "Both TotalDevice and TotalYeild can't be zero");
+        require(_amount / expend > 0, 'You have least one device');
 
         usdc.transferFrom(msg.sender, address(this), _amount);
 
         if(_invitation != address(0)) {
-            yieldOfUser[_invitation] += _amount / 100;
+            commission[_invitation] += _amount / 100;
         }
 
         uint8 lockDuration = _method == true ? 30 : 15;
         Order memory newOrder = Order(msg.sender, _amount, totalYeild, block.timestamp + lockDuration * 1 days, current_totoalDevice, current_totalYeild, lockDuration);
 
         orders.push(newOrder);
-        balanceOf[msg.sender] += _amount;
+        balanceOf[_method][msg.sender] += _amount;
         uint orderAmounts = orderOfUser[msg.sender];
         orderInUserList[orders.length - 1] = orderAmounts;
         orderOfUser[msg.sender] = orderAmounts + 1;
@@ -94,9 +87,9 @@ contract Stake is Ownable, ReentrancyGuard {
     }
 
     // 提取收益
-    function getYeild() public nonReentrant {
-        _countYield();       
-        uint amount = yieldOfUser[msg.sender];
+    function getYeild(bool _method) public nonReentrant {
+        _countYield(_method);       
+        uint amount = yieldOfUser[_method][msg.sender];
 
         require(amount > 5e19, 'Should exceed 50 USDC');
         uint amountUser = amount * 97 / 100;      // 3% 手续费
@@ -105,86 +98,115 @@ contract Stake is Ownable, ReentrancyGuard {
         emit GetReward(msg.sender, amountUser);
     }
 
-    function withdraw() public nonReentrant {
+    function withdraw(bool _method) public nonReentrant {
         uint amount;
+        uint expend = _method == true ? expend_30 : expend_15;
+        uint8 lockTime = _method == true ? 30 : 15;
         uint len = orderOfUser[msg.sender];
         for(uint i = 0; i < len; i++) {
             uint index = indexOfUser[msg.sender][i];
             Order storage order = orders[index];
+            if(order.lockDuration != lockTime) {
+                continue;
+            }
             //如果到达质押时间，但未计算收益
             if(block.timestamp > order.endTime && order.updateTime < order.endTime) {
                 uint time = Math.min(order.endTime, block.timestamp);
-                uint yield = time.sub(order.updateTime).div(1 days).mul(order.yieldPerDay).div(2);
+                uint numerator = order.amount * order.current_totalYeild * time;
+                uint denominators = expend * order.current_totalDevice * 2 * lockTime;
+                uint yield = numerator / denominators;
                 order.updateTime = time;
-                yieldOfUser[msg.sender] = yieldOfUser[msg.sender].add(yield);
-                amount.add(order.amount);
+                yieldOfUser[_method][msg.sender] += yield;
+                amount += order.amount;
                 //更新数组
                 _removeOrder(index);
             } else if (block.timestamp > order.endTime && order.updateTime == order.endTime) {//如果到达质押时间，已经计算收益
                 //更新数组
-                amount.add(order.amount);
+                amount += order.amount;
                 _removeOrder(index);
             }
         }
-        uint amountUser = amount.mul(97).div(100);      // 3% 手续费
-        yeildOfAdmin = yeildOfAdmin.add(amount.sub(amountUser));
+        uint amountUser = amount * 97 / 100;      // 3% 手续费
+        yeildOfAdmin += (amount - amountUser);
         usdc.transfer(msg.sender, amountUser);
         emit Withdraw(msg.sender, amountUser);
     }
 
     //计算收益
-    function _countYield() private {
+    function _countYield(bool _method) private {
+        uint expend = _method == true ? expend_30 : expend_15;
+        uint8 lockTime = _method == true ? 30 : 15;
         for(uint i = 0; i < orderOfUser[msg.sender]; i++) {
             uint index = indexOfUser[msg.sender][i];
             Order storage order = orders[index];
+            if(order.lockDuration != lockTime) {
+                continue;
+            }
             //判断是否已经计算了收益
             if(order.updateTime == order.endTime) {
                 continue;
             }
             uint time = Math.min(order.endTime, block.timestamp);
-            uint yield = time.sub(order.updateTime).div(1 days).mul(order.yieldPerDay).div(2);
+            uint numerator = order.amount * order.current_totalYeild * time;
+            uint denominators = expend * order.current_totalDevice * 2 * lockTime;
+            uint yield = numerator / denominators;
             order.updateTime = time;
-            yieldOfUser[msg.sender] = yieldOfUser[msg.sender].add(yield);
+            yieldOfUser[_method][msg.sender] +=yield;
         }
     }
 
     // 用户查看计算收益
-    function calReward() view public returns (uint) {
+    function calReward(bool _method) view public returns (uint) {
         uint reward;
+        uint expend = _method == true ? expend_30 : expend_15;
+        uint8 lockTime = _method == true ? 30 : 15;
         for(uint i = 0; i < orderOfUser[msg.sender]; i++) {
             uint index = indexOfUser[msg.sender][i];
-            Order storage order = orders[index];
+            Order memory order = orders[index];
             //判断是否已经计算了收益
             if(order.updateTime == order.endTime) {
                 continue;
             }
             uint time = Math.min(order.endTime, block.timestamp);
-            uint yield = time.sub(order.updateTime).div(1 days).mul(order.yieldPerDay).div(2);
+            uint numerator = order.amount * order.current_totalYeild * time;//time是实际质押的时间
+            uint denominators = expend * order.current_totalDevice * 2 * lockTime;//lockTime是原本要质押的时间
+            uint yield = numerator / denominators;
             reward += yield;
         }
         return reward;
     }
 
+    function getCommission() public returns(bool) {
+        uint amount = checkCommission();
+        require(amount > 5e19, 'Should exceed 50 USDC');
+        usdc.transfer(msg.sender, amount);
+        return true;
+    }
+
     function _removeOrder(uint _indexInOrder) private {       
         //将orders数组进行更新
-        Order memory lastOrder = orders[orders.length.sub(1)];
+        Order memory lastOrder = orders[orders.length - 1];
         Order memory order = orders[_indexInOrder];
         orders[_indexInOrder] = lastOrder;        
         //不对indexInUser中下架的Order进行删除，而是将用户持有的订单量减少，采用覆盖的方式进行维护
         uint index = orderInUserList[_indexInOrder];
         address user = order.user;
-        indexOfUser[user][index] = indexOfUser[user][orderOfUser[user].sub(1)];
-        orderOfUser[user] = orderOfUser[user].sub(1);
+        indexOfUser[user][index] = indexOfUser[user][orderOfUser[user] - 1];
+        orderOfUser[user] = orderOfUser[user] - 1;
         //维护orderInUser
-        uint lastOrderInUser = orderInUserList[orders.length.sub(1)];
+        uint lastOrderInUser = orderInUserList[orders.length - 1];
         indexOfUser[lastOrder.user][lastOrderInUser] = _indexInOrder;
         orderInUserList[_indexInOrder] = lastOrderInUser;
-        delete orderInUserList[orders.length.sub(1)];
+        delete orderInUserList[orders.length - 1];
         orders.pop(); 
     }
 
-    function ckeckYeild() public view returns(uint Yeild) {
-        return yieldOfUser[msg.sender];
+    function ckeckYeild(bool _method) public view returns(uint Yeild) {
+        return yieldOfUser[_method][msg.sender];
+    }
+
+    function checkCommission() view public returns(uint) {
+        return commission[msg.sender];
     }
 
     function setTotalYeild(uint _amount) public onlyOwner() returns(bool) {
@@ -197,9 +219,9 @@ contract Stake is Ownable, ReentrancyGuard {
         return true;
     }
 
-    function _getTotalDeviceAndYield() private returns (uint _totalDevice, uint _totalYield) {
+    function _getTotalDeviceAndYield() private view returns (uint _totalDevice, uint _totalYield) {
         _totalDevice = totalDevice;
-        _totalYeild = totalYeild;
+        _totalYield = totalYeild;
     }
 
     function withdrawByAdmin(address _to) public onlyOwner() {
